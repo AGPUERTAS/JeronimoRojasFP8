a41b69e442d1f0a3a211ebb36d1a45cafde3b400
Jeronimo Rojas Rodriguez <jeronimo.rojasr.col@upb.edu.co>
2025-08-01 09:31:11 -0500

    Vaariables, tipos de datos, constantes

--- 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
+++ 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
@@ -10,6 +10,15 @@ namespace _1.InicioVisualStudio
     {
         static void Main(string[] args)
         {
+            //Declaracion e inicializacion de variables
+            string nombre="Jeronimo";
+            const string iva = "19%";
+            string apellidos = "Rojas Rodriguez";
+            Console.Write("Bienvenido al curso de fundamentos de programaccion\n");
+            nombre = "alejandro";
+            //iva = "20%"; no puedo cambiar el valor de iva por ser una constante
+            Console.Write("\t" + nombre + " " +  apellidos +" " + iva);
+            Console.ReadKey();
         }
     }
 }
