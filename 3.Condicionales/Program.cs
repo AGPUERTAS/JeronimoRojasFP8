e3dbafae2b174a34ed17333579ef57f5f0eca0f1
Jeronimo Rojas Rodriguez <jeronimo.rojasr.col@upb.edu.co>
2025-08-09 22:28:56 -0500

    Ejercicio condicionales

--- 3.Condicionales/Program.cs
+++ 3.Condicionales/Program.cs
@@ -22,10 +22,18 @@
                 Console.WriteLine($"la persona {nombre}, debe abonar impuestos");
                 
             }
-            
+
             //2.Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, se debe mostrar el siguiente mensaje: “Bienvenido a mi sitio web”.
-            
-            
+
+            int edad = 0;
+
+            Console.WriteLine("Ingrese su edad");
+            edad = Convert.ToInt32 (Console.ReadLine());
+
+            if (edad > 18)
+            {
+                Console.WriteLine("Bienvenido a mi sitio web");
+            }
         }
     }
 }
