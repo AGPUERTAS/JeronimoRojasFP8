702dd0761b6cc376359eefa76f5038d0269ca044
Jeronimo Rojas Rodriguez <jeronimo.rojasr.col@upb.edu.co>
2025-09-03 08:26:56 -0500

    while

--- 10.While2/Program.cs
+++ 10.While2/Program.cs
@@ -4,23 +4,38 @@
     {
         static void Main(string[] args)
         {
-            //Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1 * 2 * 3 * 4 * 5)
+            /*Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1 * 2 * 3 * 4 * 5)
 
-            int contador = 1;
-            int acumulador = 1;
+            int contador = 1; es una variable entera
+            int acumulador = 1; es una variable numerica
             int num = 0;
 
             Console.WriteLine("Por favor ingrese el numero al que desa calcularle el factorial");
             num=Convert.ToInt32(Console.ReadLine());
 
-            while (contador>=num)
+             while (contador>=num)
             {
                 acumulador *= contador;
                 Console.WriteLine($"Contador: {contador} - Acumulador: {acumulador}");
                 contador++;
             }
+            
+            Console.WriteLine("el total delactorial es " + num);*/
 
-            Console.WriteLine("el total del factorial es " + num);
+            //12.Realizar algoritmo que permita obtener la suma de todos los números impares hasta el 99.
+
+            int contador = 1;
+            int suma = 0;
+
+            while (contador <= 99)
+            {
+                suma += contador;
+                Console.WriteLine($"{contador} - {suma}");
+                contador += 2;
+            }
+
+            Console.WriteLine($"La suma de los numeros impares hasta el 99 es: {suma}");
+
         }
     }
 }
