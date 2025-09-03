c24cabcae575af8c2b87f616cdd7ce5cf4a78f7d
Jeronimo Rojas Rodriguez <jeronimo.rojasr.col@upb.edu.co>
2025-09-03 08:09:40 -0500

    While

---
+++ 10.While2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _10.While2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1 * 2 * 3 * 4 * 5)
+
+            int contador = 1;
+            int acumulador = 1;
+            int num = 0;
+
+            Console.WriteLine("Por favor ingrese el numero al que desa calcularle el factorial");
+            num=Convert.ToInt32(Console.ReadLine());
+
+            while (contador>=num)
+            {
+                acumulador *= contador;
+                Console.WriteLine($"Contador: {contador} - Acumulador: {acumulador}");
+                contador++;
+            }
+
+            Console.WriteLine("el total del factorial es " + num);
+        }
+    }
+}
---
+++ 9.cicloWhile/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _9.cicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            int acumulador = 0;
+
+            while (contador <= 5)
+            {
+                acumulador += contador;
+                Console.WriteLine($"Contador: {contador} - Acumulador: {acumulador}");
+                contador++;
+            }
+
+            Console.WriteLine("La suma de los primeros 5 mnumeros enteros es: " + acumulador);
+        }
+    }
+}
