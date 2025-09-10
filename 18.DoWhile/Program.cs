c77c55204cf9d42e3a89e0bc9cfc9dc7fc41a8dd
Jeronimo Rojas Rodriguez <jeronimo.rojasr.col@upb.edu.co>
2025-09-10 09:18:44 -0500

    DoWhile

---
+++ 18.DoWhile/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _18.DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*18.Algoritmo que le pida a un grupo de usuarios su nombre, número de cuenta y saldo; para cada usuario se debe hacer lo siguiente: 
+            1. Si el saldo es mayor a 3’000.000, sacar un mensaje que diga el nombre del usuario, el número de cuenta, el saldo y “Es apto     para el crédito” 
+           2. Si el saldo es menor a 3’000.000, sacar un mensaje que diga el nombre del usuario, el número de cuenta, el saldo y “No es apto   para el crédito” 
+             El algoritmo debe permitir preguntar la información de usuarios, hasta que se le indique que ya no se desea preguntar más. 
+             Además, el algoritmo debe permitir mostrar el número de usuarios a los que se le pregunto por la información, y debe mostrar el promedio de los saldos.*/
+
+            int usuario = 0;
+            int numCuenta = 0;
+            int saldo = 0;
+            string respuesta=" ";
+
+            do
+            {
+                Console.WriteLine("Nombre del usuario");
+                usuario = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Por favor ingrese el numero de cuenta");
+                numCuenta=int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese el saldo de dicha cuenta");
+                saldo=int.Parse(Console.ReadLine());
+
+                do
+                {
+
+                }while
+
+            } while (respuesta);
+           
+
+
+
+        }
+    }
+}
---
+++ 20.DoWhile/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.Cryptography;
+
+namespace _20.DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*20.Un equipo de futbol jugó 30 partidos en el año, se desea crear un algoritmo que permita saber:
+            El número de partidos perdidos
+            El número de partidos empatados
+            El número de partidos ganados
+            El porcentaje de partidos perdidos
+            El porcentaje de partidos empatados
+            El porcentaje de partidos ganados*/
+
+            int numeroDePartidos = 0;
+            int partidosPerdidos = 0;
+            int partidosEmpatados = 0;
+            int partidosGanados = 0;
+            int cantidad = 5;
+            int respuesta = 0;
+
+            do
+            {
+                Console.WriteLine("¿Gano el partido?");
+                Console.WriteLine("1.si   2.no   3.Empate");
+                respuesta=int.Parse(Console.ReadLine());
+
+                if (respuesta == 1)
+                {
+                    partidosGanados++;
+                }
+                else if (respuesta == 2)
+                {
+                    partidosPerdidos++;
+                }
+                else
+                {
+                    partidosEmpatados++;
+                }
+
+                numeroDePartidos++;
+            } while (numeroDePartidos<cantidad);
+
+            Console.WriteLine($"De los, {cantidad} partidos gano {partidosGanados}, perdio {partidosPerdidos} y empato {partidosEmpatados}");
+
+            Console.WriteLine($"Partidos Ganados - {((100*partidosGanados)/cantidad)}, Partdios Perdidos - {((100*partidosPerdidos)/cantidad)}, Partidos Empatados - {((100*partidosEmpatados)/cantidad)}");
+            
+            
+        }
+    }
+}
