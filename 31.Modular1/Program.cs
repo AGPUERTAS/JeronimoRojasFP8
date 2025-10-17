d7a61d53912daa1ef04a2f5dab027da5a34eebc5
AGPUERTAS <jeronimo.rojasr.col@upb.edu.co>
2025-10-17 07:06:16 -0500

    Clases

--- 31.Modular1/Program.cs
+++ 31.Modular1/Program.cs
@@ -4,12 +4,68 @@
     {
         static void Main(string[] args)
         {
-            int[,] multiplosD5 = new int[CapturarFilas(), CapturarColumnas()];
+            int[,] multiplosDe5 = new int[CapturarFilas(), CapturarColumnas()];
+            LlenarMatriz(multiplosDe5);
+            MorMatriz(multiplosDe5);
+        }   
 
+        static void BorrarPantalla()
+        {
+            Console.Clear();
+        }
+
+        static void MorMatriz(int[,] matriz)
+        {
+            BorrarPantalla();
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for(int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write($"{matriz[i,j]}   |" );
+                }
+                Console.WriteLine();
+            }
+        }
+        static int[,] LlenarMatriz(int[,] matriz)
+        {
+            int numero = 0;
+
+            for(int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    do
+                    {
+                     BorrarPantalla();
+                     Console.WriteLine("Digite un numero para ingresar a la matriz: ");
+                     numero= int.Parse(Console.ReadLine());
+
+                    }while(EsMultiplo5(numero) == false);
+
+                    matriz[i, j] = numero;
+                }
+            }
+
+
+            return matriz;
+        }   
+
+        static bool EsMultiplo5(int numero)
+        {
+            if(numero % 5 == 0 && numero!=0)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Error, debe digitar un numero multiplo de 5 ");
+                return false;
+            }
         }
 
         static int CapturarFilas()
         {
+            BorrarPantalla();
             Console.WriteLine("Ingrese el numero de filas para la matriz");
             int filas=Convert.ToInt32(Console.ReadLine());
             return filas;
@@ -17,15 +73,11 @@
 
         static int CapturarColumnas()
         {
+            BorrarPantalla();
             Console.WriteLine("Ingrese el numero de columna para la matriz");
             int columnas = Convert.ToInt32(Console.ReadLine());
             return columnas;
         }
 
-        static int[,] LlenarMatriz(int[,] matriz)
-        {
-
-            return matriz;
-        }   
     }
 }
---
+++ 33.TallerProgramacionModular/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _33.TallerProgramacionModular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] palabras = new string[CapturarColumnas().Length];
+            LlenarVector(palabras);
+            MostrarVector(palabras);
+        }
+        static void MostrarVector(string[] vector)
+        {
+            for(int i = 0; i < vector.Length; i++)
+            {
+                Console.Write($"{vector[i]} |");
+            }
+            Console.WriteLine();
+        }
+
+        static string[] LlenarVector(string[] vector)
+        {
+            string palabra;
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine($"Por favor ingrese la palabra deseada para ingresar al vector");
+                palabra = Console.ReadLine();
+            }
+            return vector;
+        }
+
+        static string CapturarColumnas()
+        {
+            Console.WriteLine("Ingrese la palabra que desea analizar");
+            string columnas = Console.ReadLine();
+            return columnas;
+        }
+    }
+}
