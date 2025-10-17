7b365b9a95f6a418a0b5f8c6ab13aff4670a6370
AGPUERTAS <jeronimo.rojasr.col@upb.edu.co>
2025-10-17 07:09:56 -0500

    Clase

--- 10.While2/Program.cs
+++ 10.While2/Program.cs
@@ -4,10 +4,10 @@
     {
         static void Main(string[] args)
         {
-            /*Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1 * 2 * 3 * 4 * 5)
+            //Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1 * 2 * 3 * 4 * 5)
 
-            int contador = 1; es una variable entera
-            int acumulador = 1; es una variable numerica
+            int contador = 1; //es una variable entera
+            int acumulador = 1; //es una variable numerica
             int num = 0;
 
             Console.WriteLine("Por favor ingrese el numero al que desa calcularle el factorial");
@@ -20,21 +20,7 @@
                 contador++;
             }
             
-            Console.WriteLine("el total delactorial es " + num);*/
-
-            //12.Realizar algoritmo que permita obtener la suma de todos los números impares hasta el 99.
-
-            int contador = 1;
-            int suma = 0;
-
-            while (contador <= 99)
-            {
-                suma += contador;
-                Console.WriteLine($"{contador} - {suma}");
-                contador += 2;
-            }
-
-            Console.WriteLine($"La suma de los numeros impares hasta el 99 es: {suma}");
+            Console.WriteLine("el total delactorial es " + num);
 
         }
     }
