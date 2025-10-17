5644c8925f8daf23e64293920defe6ed04f15f9a
AGPUERTAS <jeronimo.rojasr.col@upb.edu.co>
2025-10-17 07:20:29 -0500

    Clase

--- 13.While5/Program.cs
+++ 13.While5/Program.cs
@@ -46,25 +46,7 @@
             Console.WriteLine($"La cantidad de numeros menores que 0 es: {menores}");
             Console.WriteLine($"La cantidad de numeros iguales que 0 es: {iguales}");
 
-            //14.Escribir un algoritmo que cuente cuántos números pares hay en un rango de números del 1 al número n.
-
-            /*int rango = 0;
-            int contadorPares = 0;
-            int contador = 1;
-
-            Console.WriteLine("Ingrese el rango a evaluar");
-            rango=int.Parse(Console.ReadLine());
-
-            while (contador<=rango)
-            {
-                if (contador % 2 == 0)
-                {
-                    contadorPares++;
-                }
-
-                contador++;
-            }
-            Console.WriteLine($"La cantidad de numeros pares desde 1 hasta, {rango} es: {contadorPares}");*/
+            
         }
     }
 }
