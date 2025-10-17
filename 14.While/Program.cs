3a1fe62664e73c7cc7a44c30e5a4d9410c27d352
AGPUERTAS <jeronimo.rojasr.col@upb.edu.co>
2025-10-17 07:21:54 -0500

    Clase

---
+++ 14.While/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _14.While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //14.Escribir un algoritmo que cuente cuántos números pares hay en un rango de números del 1 al número n.
+
+            int rango = 0;
+            int contadorPares = 0;
+            int contador = 1;
+
+            Console.WriteLine("Ingrese el rango a evaluar");
+            rango=int.Parse(Console.ReadLine());
+
+            while (contador<=rango)
+            {
+                if (contador % 2 == 0)
+                {
+                    contadorPares++;
+                }
+
+                contador++;
+            }
+            Console.WriteLine($"La cantidad de numeros pares desde 1 hasta, {rango} es: {contadorPares}");
+        }
+    }
+}
